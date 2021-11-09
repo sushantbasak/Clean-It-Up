f705eab86ea48fd64c3e10208d8e8860d09ab8a4
sushantbasak <sushantbasak73@gmail.com>
2021-11-09 22:49:35 +0530

    feat: Add User Queries & Transfer it to DBStore

--- backend/api/dbstore/mongo/index.js
+++ backend/api/dbstore/mongo/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const appSettings = require('../../../config');
 const mongoDB = appSettings.mongoDb;
 
+const { createUser, findUser, updateUser } = require('./queries/user');
+
 mongoose.connect(
   mongoDB,
   {
@@ -14,5 +16,11 @@ mongoose.connect(
   },
   () => console.log(`Database Connected on ${mongoDB}`)
 );
+
+const dbStoreHandler = {
+  createUser,
+  findUser,
+  updateUser,
+};
 
 module.exports = dbStoreHandler;
