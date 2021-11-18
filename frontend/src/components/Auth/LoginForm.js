b556f02a914caae0043dfde082a14309d7a22645
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-19 01:48:00 +0530

    feat: add login form

--- frontend/src/components/Auth/LoginForm.js
+++ frontend/src/components/Auth/LoginForm.js
@@ -3,9 +3,32 @@ import './LoginForm.css';
 
 const LoginForm = () => {
   return (
-    <form>
-      <h1>Login Form ...</h1>
-    </form>
+    <div className="login-form">
+      {' '}
+      <form>
+        <div className="form-inputs">
+          <div className="form-input">
+            <label>Title: </label>
+            <input type="text" name="title" required />
+          </div>
+          <div className="form-input">
+            <label>Address: </label>
+            <input type="text" name="address" required />
+          </div>
+          <div className="form-input">
+            <label>Description: </label>
+            <textarea rows="5" name="description" required />
+          </div>
+          <div className="form-input">
+            <label>Image:</label>
+            <input type="file" id="myFile" name="image" />
+          </div>
+        </div>
+        <div className="form-submit">
+          <button type="submit">Post Job</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
