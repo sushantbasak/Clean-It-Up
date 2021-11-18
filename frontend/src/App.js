58524318a127c2edf21a9bf4058119de766fe29e
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-18 14:48:25 +0530

    feat: import view in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import React from 'react';
 import Home from './views/Home/Home';
 import About from './views/About/About';
 import UserDashboard from './views/UserDashboard/UserDashboard';
+import WorkerDashboard from './views/WorkerDashboard/WorkerDashboard';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/UserDashboard" element={<UserDashboard />} />
+        <Route exact path="/WorkerDashboard" element={<WorkerDashboard />} />
       </Routes>
     </div>
   );
