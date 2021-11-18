86084661ff6732200dd618c2c1810106ee1f6177
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-18 18:49:19 +0530

    feat: add dummy jobs data

--- frontend/src/views/AdminDashboard/AdminDashboard.js
+++ frontend/src/views/AdminDashboard/AdminDashboard.js
@@ -2,9 +2,32 @@ import React from 'react';
 import './AdminDashboard.css';
 
 import Navbar from '../../components/Navbar/Navbar';
-import Navbar from '../../components/JobList/JobList';
+import JobList from '../../components/Jobs/JobList';
 
 const AdminDashboard = () => {
+     const jobs = [
+    {
+      id: 1,
+      title: 'Cleaning Job 1',
+      description: 'Please clean the garbage',
+      address: 'At Chandani Chowk, Delhi',
+      status: 1
+    },
+    {
+      id: 2,
+      title: 'Cleaning Job 2',
+      description: 'Clean the garbage properly',
+      address: 'At Vasant Vihar, Delhi',
+      status: 1
+    },
+    {
+      id: 3,
+      title: 'Cleaning Job 3',
+      description: 'Clear blocked sewere',
+      address: 'At Pitampura Delhi',
+      status: 0
+    },
+  ];
     return (
         <>
         <Navbar />
