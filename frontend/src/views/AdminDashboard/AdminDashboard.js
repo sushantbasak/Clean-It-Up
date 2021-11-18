f5d0efa17915cda11db13493488c6a6c100ec6a3
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-18 18:37:46 +0530

    feat: setup react on admin dashboard

---
+++ frontend/src/views/AdminDashboard/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AdminDashboard.css';
+
+const AdminDashboard = () => {
+    return (
+        <>
+        <h2>Admin Dashboard </h2>
+        </>
+    )
+}
+
+export default AdminDashboard;
