cf9bb19d6928fbe30dd08abd70585e8e673d9907
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-18 18:47:00 +0530

    feat: add job list to admin dashboard

--- frontend/src/views/AdminDashboard/AdminDashboard.js
+++ frontend/src/views/AdminDashboard/AdminDashboard.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './AdminDashboard.css';
 
 import Navbar from '../../components/Navbar/Navbar';
+import Navbar from '../../components/JobList/JobList';
 
 const AdminDashboard = () => {
     return (
         <>
         <Navbar />
+        <JobList role="1" list={jobs} />
         </>
     )
 }
