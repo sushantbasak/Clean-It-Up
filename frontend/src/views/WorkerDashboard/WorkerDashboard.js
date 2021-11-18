1f5da2f031ebcc9fa0559ef44d0e5c9e5e0b8607
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-18 14:44:51 +0530

    feat: worker dashboard react setup

---
+++ frontend/src/views/WorkerDashboard/WorkerDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WorkerDashboard.css';
+
+const WorkerDashboard = () => {
+    return (
+        <>
+        <h1>WorkerDashbord</h1>
+        </>
+    )
+} 
+
+export default WorkerDashboard;
