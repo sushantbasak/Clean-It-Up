6389b2a922f10e2ac0fcc60371060fc250be015c
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-19 13:15:16 +0530

    style: change home page ui

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -9,11 +9,30 @@ const Home = () => {
       <Navbar />
       <h1>Home Page</h1>
       <p>This is the home Page</p>
-      <Link to="/UserDashboard">User Dashboard</Link>
-      <br />
-      <Link to="/WorkerDashboard">Worker Dashboard</Link>
-      <br />
-      <Link to="/AdminDashboard">Admin Dashboard</Link>
+      <div className="main">
+        <div className="block">
+          <Link to="/UserDashboard">User Dashboard</Link>
+          <br />
+          <Link to="/UserLogin">User Login</Link>
+          <br />
+          <Link to="/UserRegister">User Register</Link>
+          <br />
+        </div>
+        <div className="block">
+          <Link to="/WorkerDashboard">Worker Dashboard</Link>
+          <br />
+          <Link to="/WorkerLogin">Worker Login</Link>
+          <br />
+          <Link to="/WorkerRegister">Worker Register</Link>
+          <br />
+        </div>
+        <div className="block">
+          <Link to="/AdminDashboard">Admin Dashboard</Link>
+          <br />
+          <Link to="/AdminLogin">Admin Login</Link>
+          <br />
+        </div>
+      </div>
     </div>
   );
 };
