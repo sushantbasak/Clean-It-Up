3c3ce21cca65f25743933fce826dbe5e9b729ec6
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-19 17:05:04 +0530

    feat: add edit profile

---
+++ frontend/src/views/Profile/EditProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './EditProfile.css';
+
+const EditProfile = () => {
+  return (
+    <>
+      <h1>Edit profile</h1>
+    </>
+  );
+};
+
+export default EditProfile;
