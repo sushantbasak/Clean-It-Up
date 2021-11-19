d3d8e8366b6c79f13923bf5ea1f0835b2d096769
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-19 21:40:09 +0530

    feat: add fields to view profile

--- frontend/src/views/Profile/Profile.js
+++ frontend/src/views/Profile/Profile.js
@@ -3,11 +3,29 @@ import './Profile.css';
 
 import Navbar from '../../components/Navbar/Navbar';
 
-const Profile = () => {
+const Profile = (props) => {
   return (
-    <>
+    <div className="profile">
       <Navbar />
-    </>
+      <div className="profile-body">
+        <img src="garbage.jpg" alt="profile" />
+        <div className="data-line">
+          <p>
+            <b>Username: </b> {props.user.username}
+          </p>
+        </div>
+        <div className="data-line">
+          <p>
+            <b>Email: </b> {props.user.email}
+          </p>
+        </div>
+        <div className="data-line">
+          <p>
+            <b>Address: </b> {props.user.address}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
