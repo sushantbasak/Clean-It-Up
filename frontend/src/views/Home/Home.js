bd548ee7935f86cd4d3779216c655a0527d96527
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-20 01:25:55 +0530

    feat: add icons on home page

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -9,26 +9,32 @@ const Home = () => {
       <Navbar />
       <div className="main">
         <div className="block">
-          {/* <Link to="/UserDashboard">User Dashboard</Link> */}
-          {/* <br /> */}
-          <Link to="/UserLogin">User Login</Link>
-          <br />
-          <Link to="/UserRegister">User Register</Link>
-          <br />
+          <h2 className="block-heading">User</h2>
+          <img src="user.png" alt="img" className="image-dash" />
+          <div className="links">
+            <Link to="/UserLogin">User Login</Link>
+            <br />
+            <Link to="/UserRegister">User Register</Link>
+            <br />
+          </div>
         </div>
         <div className="block">
-          {/* <Link to="/WorkerDashboard">Worker Dashboard</Link> */}
-          {/* <br /> */}
-          <Link to="/WorkerLogin">Worker Login</Link>
-          <br />
-          <Link to="/WorkerRegister">Worker Register</Link>
-          <br />
+          <h2 className="block-heading">Worker</h2>
+          <img src="user.png" alt="img" className="image-dash" />
+          <div className="links">
+            <Link to="/WorkerLogin">Worker Login</Link>
+            <br />
+            <Link to="/WorkerRegister">Worker Register</Link>
+            <br />
+          </div>
         </div>
         <div className="block">
-          {/* <Link to="/AdminDashboard">Admin Dashboard</Link> */}
-          {/* <br /> */}
-          <Link to="/AdminLogin">Admin Login</Link>
-          <br />
+          <h2 className="block-heading">Admin</h2>
+          <img src="user.png" alt="img" className="image-dash" />
+          <div className="admin-links">
+            <Link to="/AdminLogin">Admin Login</Link>
+            <br />
+          </div>
         </div>
       </div>
     </div>
