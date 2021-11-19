1b77009906146596a05f813bee257416805382eb
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-20 00:55:33 +0530

    fix: remove dashboard links

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -7,28 +7,26 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <h1>Home Page</h1>
-      <p>This is the home Page</p>
       <div className="main">
         <div className="block">
-          <Link to="/UserDashboard">User Dashboard</Link>
-          <br />
+          {/* <Link to="/UserDashboard">User Dashboard</Link> */}
+          {/* <br /> */}
           <Link to="/UserLogin">User Login</Link>
           <br />
           <Link to="/UserRegister">User Register</Link>
           <br />
         </div>
         <div className="block">
-          <Link to="/WorkerDashboard">Worker Dashboard</Link>
-          <br />
+          {/* <Link to="/WorkerDashboard">Worker Dashboard</Link> */}
+          {/* <br /> */}
           <Link to="/WorkerLogin">Worker Login</Link>
           <br />
           <Link to="/WorkerRegister">Worker Register</Link>
           <br />
         </div>
         <div className="block">
-          <Link to="/AdminDashboard">Admin Dashboard</Link>
-          <br />
+          {/* <Link to="/AdminDashboard">Admin Dashboard</Link> */}
+          {/* <br /> */}
           <Link to="/AdminLogin">Admin Login</Link>
           <br />
         </div>
