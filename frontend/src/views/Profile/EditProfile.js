cd84c243264bc7fc7a669ce08574dcf82f6dfd09
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-19 17:13:19 +0530

    feat: create form for editing

--- frontend/src/views/Profile/EditProfile.js
+++ frontend/src/views/Profile/EditProfile.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './EditProfile.css';
+import { Link } from 'react-router-dom';
 
-const EditProfile = () => {
+const EditProfile = (props) => {
   return (
     <>
-      <h1>Edit profile</h1>
+      <div className="login-form">
+        <h2 className="heading"> Edit Profile </h2>
+        <form method="POST">
+          <div className="form-inputs">
+            <div className="form-input">
+              <label>Username: </label>
+              <input type="text" name="username" required value={props.username} />
+            </div>
+            <div className="form-input">
+              <label>Address: </label>
+              <input type="text" name="address" required value={props.address} />
+            </div>
+          </div>
+          <div className="form-submit">
+            <button className="back">
+              <Link to="/">Back</Link>
+            </button>
+            <button type="submit" className="submit-btn">
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
