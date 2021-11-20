eb46702ff93024ff6257f9964306ab93612013b6
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-20 16:33:12 +0530

    feat: add new img sources

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -7,17 +7,17 @@ const InfoCard = () => {
   return (
     <Carousel className="crousel" variant="dark">
       <Carousel.Item interval={3000} className="card-item">
-        <img src="garbage.jpg" alt="garbage" />
+        <img src="india.png" alt="garbage" />
         <h3>Our Aim</h3>
         <p>Our aim is to make the India the cleanest country in the world.</p>
       </Carousel.Item>
       <Carousel.Item interval={3000} className="card-item">
-        <img src="garbage.jpg" alt="garbage" />
+        <img src="disease.png" alt="garbage" />
         <h3>Our motivation</h3>
         <p>Our motivation is cleanliness will eleminate all diseases.</p>
       </Carousel.Item>
       <Carousel.Item interval={3000} className="card-item">
-        <img src="garbage.jpg" alt="garbage" />
+        <img src="ethic.jpg" alt="garbage" />
         <h3>Our Ethics</h3>
         <p>Our ethics are to complete a registered job in time.</p>
       </Carousel.Item>
