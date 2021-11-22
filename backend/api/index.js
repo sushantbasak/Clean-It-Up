c5a089f4f10fa7e584f8fcac7ed2355591d52edd
sushantbasak <sushantbasak73@gmail.com>
2021-11-22 13:04:31 +0530

    feat: Add API Health Checks

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,4 +4,8 @@ const router = require('express').Router();
 
 router.use('/v1', require('./v1'));
 
+router.get('/health', (req, res) => {
+  res.send({ status: 'UP' });
+});
+
 module.exports = router;
