f1598778167e9540a338a8cc631f336382f45c8f
sushantbasak <sushantbasak73@gmail.com>
2021-11-22 12:52:21 +0530

    feat: Add Deployments other than Developement in ENV

--- backend/config/index.js
+++ backend/config/index.js
@@ -17,6 +17,6 @@ const config = {
   },
 };
 
-const appSettings = { ...config['development'] };
+const appSettings = { ...config[process.env.NODE_ENV || 'development'] };
 
 module.exports = appSettings;
