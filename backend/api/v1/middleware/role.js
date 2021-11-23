12000344682db80cec96605bf90c04f2a3141620
sushantbasak <sushantbasak73@gmail.com>
2021-11-23 11:26:06 +0530

    feat: Add User Access Only Permission Middleware

--- backend/api/v1/middleware/role.js
+++ backend/api/v1/middleware/role.js
@@ -21,4 +21,14 @@ const restrictWorker = (req, res, next) => {
   next();
 };
 
-module.exports = { adminProtect, restrictWorker };
+const userAccessOnly = (req, res, next) => {
+  if (req.user.role !== 0)
+    return res.sendError(
+      httpCode.StatusCodes.UNAUTHORIZED,
+      MESSAGES.api.UNAUTHORIZED_USER
+    );
+
+  next();
+};
+
+module.exports = { adminProtect, restrictWorker, userAccessOnly };
