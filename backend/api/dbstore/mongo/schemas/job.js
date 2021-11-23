cfa4eddb90994f62b297bd9c183cef62fc5e629b
sushantbasak <sushantbasak73@gmail.com>
2021-11-23 11:08:37 +0530

    feat: Create Job Schema

---
+++ backend/api/dbstore/mongo/schemas/job.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const { COLLECTIONS } = require('../../../../constants');
+
+const jobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please add First name'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add Last name'],
+  },
+  pincode: {
+    type: Number,
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add Addresss'],
+  },
+  postedBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: COLLECTIONS.USER,
+    required: true,
+  },
+  assignedTo: {
+    type: mongoose.Schema.ObjectId,
+    ref: COLLECTIONS.USER,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = {
+  schema: mongoose.model(COLLECTIONS.JOB, jobSchema),
+};
