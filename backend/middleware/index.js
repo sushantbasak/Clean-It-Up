8868aefd34fd7aa9c4d4bb2c769bf781a6f144e3
SamarpreetSingh <samar.guraya18@gmail.com>
2021-11-23 18:32:00 +0530

    feat: connect edit profile with api

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const appSettings = require('../config');
-const api = require('../api');
-const bodyParser = require('body-parser');
-const { errors, isCelebrateError } = require('celebrate');
-const { apiResponseGenerator } = require('../init/bootstrap');
-const compression = require('compression');
+const express = require("express");
+const appSettings = require("../config");
+const api = require("../api");
+const bodyParser = require("body-parser");
+const { errors, isCelebrateError } = require("celebrate");
+const { apiResponseGenerator } = require("../init/bootstrap");
+const compression = require("compression");
 const middleware = async () => {
   const app = express();
 
   // enable cross domain access
   app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Methods',
-      'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PUT,POST,DELETE,PATCH"
     );
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, x-access-token, visitorid, tempsave'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-    res.header('Access-Control-Allow-Origin', '*');
-
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
   });
 
@@ -37,11 +37,11 @@ const middleware = async () => {
   );
 
   // parse application/json
-  app.use(bodyParser.json({ limit: '30mb' }));
+  app.use(bodyParser.json({ limit: "30mb" }));
   app.use(apiResponseGenerator);
 
   // connect to api
-  app.use('/api', api);
+  app.use("/api", api);
   app.use((err, req, res, next) => {
     if (isCelebrateError(err)) {
       console.log(err);
