d2103fd5f5198bad6eaa36de5138e330b28f1afe
sushantbasak <sushantbasak73@gmail.com>
2021-11-27 11:46:39 +0530

    feat: Add File Upload Controller

--- backend/api/v1/controllers/uploadController.js
+++ backend/api/v1/controllers/uploadController.js
@@ -3,6 +3,7 @@
 const router = require('express').Router();
 const httpCode = require('http-status-codes');
 const { celebrate } = require('celebrate');
+const sharp = require('sharp');
 
 // Services
 
@@ -10,6 +11,8 @@ const ErrorHandler = require('../../utils/errorHandler');
 const uploadService = require('../services/uploadService');
 const { MESSAGES } = require('../../../constants');
 const { imageUpload } = require('../middleware/upload');
+const { imageCompression } = require('../services/fileCompression');
+const { protect } = require('../middleware/auth');
 
 // Functions
 
@@ -17,14 +20,34 @@ const getFileUpload = (req, res) => {
   res.sendSuccess(MESSAGES.api.CREATED, httpCode.StatusCodes.CREATED);
 };
 
-const uploadFile = (req, res) => {
-  // console.log(req.file.buffer);
+const uploadFile = async (req, res) => {
+  try {
+    const compressedImage = await imageCompression(req.file.buffer);
 
-  res.sendSuccess(MESSAGES.api.CREATED, httpCode.StatusCodes.CREATED);
+    if (compressedImage.status === 'ERROR_FOUND')
+      throw new Error('Unable to compress Images');
+
+    const uploadResult = uploadService.createFileUpload({
+      file: compressedImage.imageBuffer,
+      uploadedBy: req.user._id,
+      fileName: req.file.originalname,
+    });
+
+    if (uploadResult.status === 'ERROR_FOUND')
+      throw new Error('Unable to create a new File Upload');
+
+    res.sendSuccess(MESSAGES.api.CREATED, httpCode.StatusCodes.CREATED);
+  } catch (ex) {
+    ErrorHandler.extractError(ex);
+    res.sendError(
+      httpCode.StatusCodes.INTERNAL_SERVER_ERROR,
+      MESSAGES.api.SOMETHING_WENT_WRONG
+    );
+  }
 };
 
 router.get('/', getFileUpload);
 
-router.post('/', imageUpload.single('avatar'), uploadFile);
+router.post('/', imageUpload.single('avatar'), protect, uploadFile);
 
 module.exports = router;
